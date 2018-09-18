f85abb3da521f0ab99184e4f6ffa24524ca49a7d
Denis Issoupov <dissoupov@salesforce.com>
2018-09-18 13:14:28 -0700

    Version v1.1.1 format

--- .project/go-project.mk
+++ .project/go-project.mk
@@ -65,7 +65,7 @@ GIT_HASH := $(shell git rev-parse --short HEAD)
 COMMITS_COUNT := $(shell git rev-list --count ${GIT_HASH})
 #
 PROD_VERSION := $(shell cat .VERSION)
-GIT_VERSION := $(shell printf %s-%d%s ${PROD_VERSION} ${COMMITS_COUNT} ${GIT_DIRTY})
+GIT_VERSION := $(shell printf %s.%d%s ${PROD_VERSION} ${COMMITS_COUNT} ${GIT_DIRTY})
 COVPATH=.coverage
 
 # List of all .go files in the project, excluding vendor and .tools
