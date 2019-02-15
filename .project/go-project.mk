13f96e03ae79ace2abcad924411ddcd524d9e9de
Hayk Baluyan <hbaluyan@salesforce.com>
2019-02-15 11:13:06 -0800

    x/tools/godoc needs package golang.org/x/net

--- .project/go-project.mk
+++ .project/go-project.mk
@@ -386,6 +386,7 @@ help:
 
 getdevtools:
 	$(call httpsclone,${GITHUB_HOST},golang/tools,           ${TOOLS_PATH}/src/golang.org/x/tools,                  release-branch.go1.11)
+	$(call httpsclone,${GITHUB_HOST},golang/net,             ${TOOLS_PATH}/src/golang.org/x/net,                    release-branch.go1.11)
 	$(call httpsclone,${GITHUB_HOST},golang/dep,             ${TOOLS_PATH}/src/github.com/golang/dep,               master)
 	$(call httpsclone,${GITHUB_HOST},go-delve/delve,         ${TOOLS_PATH}/src/github.com/go-delve/delve,           master)
 	$(call httpsclone,${GITHUB_HOST},uudashr/gopkgs,         ${TOOLS_PATH}/src/github.com/uudashr/gopkgs,           master)
